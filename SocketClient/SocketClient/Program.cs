88ad229e71b0d63981adeee33a48ab891c7b8d8c
Lrr <921090271@qq.com>
2017-06-30 23:33:47 +0800

    在客户端用了BeginReceive方法进行异步的消息侦听，如果收到就打印出来

--- SocketClient/SocketClient/Program.cs
+++ SocketClient/SocketClient/Program.cs
@@ -15,7 +15,16 @@ namespace SocketClient
             //连接到指定服务器的指定端口
             socket.Connect("localhost", 4530);
 
-            
+            //实现消息接收的方法
+
+            var buffer = new byte[1024]; //设置一个缓冲区，保存数据
+            socket.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, new AsyncCallback((ar) =>
+            {
+                var length = socket.EndReceive(ar);
+                var message = Encoding.Unicode.GetString(buffer, 0, length);
+                Console.WriteLine(message);
+            }), null);
+
             Console.WriteLine("connect to the server");
             Console.Read();
         }
