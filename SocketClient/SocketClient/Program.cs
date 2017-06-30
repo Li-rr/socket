439b95146af4d24df6925233e4440c29a7ef1b1e
Lrr <921090271@qq.com>
2017-06-30 23:25:58 +0800

    socket编程学习

---
+++ SocketClient/SocketClient/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+namespace SocketClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //创建一个socket
+            var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            //连接到指定服务器的指定端口
+            socket.Connect("localhost", 4530);
+
+            
+            Console.WriteLine("connect to the server");
+            Console.Read();
+        }
+    }
+}
---
+++ socketServer/socketServer/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace socketServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //创建一个新的socket,这里使用最常用的基于TCP的Stream Socket(流式套接字)
+            var socket = new Socket(AddressFamily.InterNetwork,SocketType.Stream,ProtocolType.Tcp);
+
+            //将该socket绑定到主机上面的某个端口
+            socket.Bind(new IPEndPoint(IPAddress.Any,4530));
+
+            //启动监听，并且设置一个最大的队列长度
+            socket.Listen(4);
+            Console.WriteLine("Server is ready!");
+            Console.Read();
+        }
+    }
+}
